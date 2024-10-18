6bf5b60b4a561f96bb31e1ccedef01cfd6dc6cc3
Aquiles Falcão <aquilesfalcaoo@gmail.com>
2024-10-18 16:26:05 -0300

    feat: criado environments e primeng configurado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  },
+  {
+    path: "login",
+    loadComponent: () =>
+      import("./pages/auth/login/login.component").then(
+        (c) => c.LoginComponent
+      ),
+  },
+  {
+    path: "dashboard",
+    loadComponent: () =>
+      import("./pages/dashboard/dashboard.component").then(
+        (c) => c.DashboardComponent
+      ),
+  },
+  {
+    path: "users",
+    loadComponent: () =>
+      import("./pages/users/users.component").then((c) => c.UsersComponent),
+  },
+];
